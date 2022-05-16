476ffa1c35e7aa9682427c6dea024a87ad0308ab
Luke Renoe <lukerenoe@gmail.com>
2022-05-16 16:45:52 -0600

    api

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $("#search-button").on("click", function() {
     var searchValue = $("#search-value").val()
     console.log(searchValue)
+    geoCode(searchValue)
 })
+
+function geoCode(searchValue) {
+    $.ajax({
+        URL: "http://api.openweathermap.org/geo/1.0/direct?q=" + searchValue + "&appid=8a19eb9f8b7949754f6b14f7fd648a12", 
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    })
+}
