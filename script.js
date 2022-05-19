37675167d208401bcfb13b7818ce36b6fa330e3a
Luke Renoe <lukerenoe@gmail.com>
2022-05-18 18:28:37 -0600

    card styles

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function geoCode(searchValue) {
     fiveDayForecast(response[0].lat, response[0].lon)
     })
 }
-
 function currentWeather(searchValue, lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + 
     "&appid=8a19eb9f8b7949754f6b14f7fd648a12&units=imperial")
@@ -41,8 +40,6 @@ else {
 }
 
 var currentDate = $("<h2>").text(moment.unix(response.current.dt).format("MM/DD/YYYY"))
-
-
 var weatherCard = $("<div>").addClass("card")
 
 weatherCard.append(cityName, currentDate, temp, humidity, uvi, windSpeed)
@@ -51,6 +48,16 @@ weatherCard.append(cityName, currentDate, temp, humidity, uvi, windSpeed)
     })
 }
 
+// function saveSearch() {
+//     var searchArray = JSON.parse (localStorage.getItem ("cities") ) || []
+//     searchArray.push(newSearch)
+//     var newSearch = $("#search-value").val()
+//     localStorage.setItem("cities", JSON.stringify(searchArray))
+//     displaySearches()
+//     console.log (saveSearch())
+//     }
+
+
 function fiveDayForecast(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + 
     "&appid=8a19eb9f8b7949754f6b14f7fd648a12&units=imperial")
@@ -58,16 +65,17 @@ function fiveDayForecast(lat, lon) {
         return data.json()
     }).then(function (response){
         console.log(response)
-for (let i = 0; i < response.daily.length-3; i++) {
+    
+for (let i = 1; i < response.daily.length-2; i++) {
     var temp = $("<p>").text("Temperature: " + response.daily[i].temp.day)
+    var humidity = $("<p>").text("Humidity: " + response.daily[i].humidity)
+    var wind = $("<p>").text("WindSpeed:" + response.daily[i].wind_speed)
     var weatherCard = $("<div>").addClass("card")
-
-weatherCard.append(temp)
+    var forecastDate = $("<p>").text(moment.unix(response.daily[i].dt).format("ddd"))
+    var iconUrl = `https://openweathermap.org/img/w/${response.daily[i].weather[0].icon}.png`
+    var icon = $("<img>").attr("src", iconUrl).addClass("sizeicon")
+weatherCard.append(forecastDate, icon, temp, humidity, wind)
         $("#fiveforecast").append(weatherCard)
 }
-
-
-
-        
-    })
+})
 }
